7c0a2f82fdc3012946021efca1be008ef17e3280
Daniel Nwafor <danidev3lops@gmail.com>
2024-07-06 03:19:32 +0000

    implement mongo, onboarding feature, profile-image upload feature, user creation feature

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,12 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+import React from "react";
+import "../globals.css";
+
 export const metadata = {
   title: "Shreads",
   description: "A Nextjs 14 Threads clone App",
 };
-
-import { ClerkProvider } from "@clerk/nextjs";
-import { Inter } from "next/font/google";
-import React from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
